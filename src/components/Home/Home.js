2ee87fe9b330d9d0ef300fbe02b87878f9ced48f
LAPTOP-J5SPII67\Acer <vikrantrath4@gmail.com>
2020-02-03 01:07:09 +0530

    Added Better description

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,7 +13,10 @@ export default function Home() {
     <section id="Home">
       <Helmet>
         <title>Sheer Analytics And Insights</title>
-        <meta name="description" content="Online presence of Sheer Analytics and Insights PVT LTD. " />
+        <meta
+          name="description"
+          content=" Sheeranalyticsandinsights Market Research Services Gives You Almost Real-Time Data Of Market Conditions, Challenges, Visualization Services For B2B All Around The World."
+        />
       </Helmet>
       <FeaturedSection />
       <Solutions />
