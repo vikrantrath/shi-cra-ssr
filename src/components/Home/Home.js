f7b7ec8da2ae6521ffed247bc174f4a6f1e81331
LAPTOP-J5SPII67\Acer <vikrantrath4@gmail.com>
2020-02-02 19:30:53 +0530

    changed head and base html

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,22 +1,27 @@
-import React from 'react'
-import FeaturedSection from './HomeComponents/FeaturedSection'
-import WhyChooseUs from './HomeComponents/WhyChooseUs'
-import Solutions from './HomeComponents/Solutions'
-import Industry from './HomeComponents/Industry'
-import LatestPublication from './HomeComponents/LatestPublication'
-import Benefits from './HomeComponents/Benefits'
-import Clients from './HomeComponents/Clients'
+import React from "react";
+import { Helmet } from "react-helmet";
+import FeaturedSection from "./HomeComponents/FeaturedSection";
+import WhyChooseUs from "./HomeComponents/WhyChooseUs";
+import Solutions from "./HomeComponents/Solutions";
+import Industry from "./HomeComponents/Industry";
+import LatestPublication from "./HomeComponents/LatestPublication";
+import Benefits from "./HomeComponents/Benefits";
+import Clients from "./HomeComponents/Clients";
 
 export default function Home() {
-    return (
-        <section id="Home">
-            <FeaturedSection />
-            <Solutions />
-            <Industry />
-            <LatestPublication />
-            <Benefits />
-            <Clients />
-            <WhyChooseUs />
-        </section>
-    )
+  return (
+    <section id="Home">
+      <Helmet>
+        <title>Sheer Analytics And Insights. Welcome to Home</title>
+        <meta name="description" content="Online presence of Sheer Analytics and Insights PVT LTD. " />
+      </Helmet>
+      <FeaturedSection />
+      <Solutions />
+      <Industry />
+      <LatestPublication />
+      <Benefits />
+      <Clients />
+      <WhyChooseUs />
+    </section>
+  );
 }
